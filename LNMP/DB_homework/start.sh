75a5452ca9f3300344752a129fe1ac33abaea653
KutsunaSubaRya <index20010928@gmail.com>
2023-05-20 19:21:54 +0800

    update start.sh to confrom phpMyAdmin

--- LNMP/DB_homework/start.sh
+++ LNMP/DB_homework/start.sh
@@ -4,3 +4,6 @@ cd ~/Desktop/2023_Learning/LNMP/DB_homework
 sudo cp -R * /usr/share/nginx/html/db_final
 sudo systemctl restart nginx
 sudo nginx -t
+sudo ln -s /usr/share/webapps/phpMyAdmin/ /usr/share/nginx/html/db_final
+sudo systemctl restart nginx
+sudo systemctl restart php-fpm
